1e0a68317e36b7422ea196a53321f59ad0c1f52b
sivaprakashS <31438994+sivaprakashS@users.noreply.github.com>
2018-02-02 16:00:35 +0530

    primenum.c

---
+++ primenum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,flag;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("prime numbers between the %d and %d",a,b );
+while(b<a)
+{
+flag=0;
+for(i=2;i<=b\2;i++)
+{
+if(b% i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+printf("%d",b);
+++low;
+}
+}
+
